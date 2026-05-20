b81d3ed2c3c73d33c4db9dd377e42542a1f759b6
williamsedou0-hub <williamsedou0@gmail.com>
2026-05-20 00:23:21 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuBtn = document.getElementById("menu-btn");
+
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+
+    mobileMenu.classList.toggle("hidden");
+
+});
